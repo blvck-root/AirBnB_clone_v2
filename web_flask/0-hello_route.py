432edbe985a344833c856c2c5bd6b1270730ea04
sanieeme <shoneesani@gmail.com>
2024-04-24 12:45:17 +0000

    s

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -5,12 +5,15 @@ script that starts a Flask web application
 
 from flask import Flask
 
+
 app = Flask(__name__)
+
 
 @app.route('/', strict_slashes=False)
 def hello():
     """displays Hello HBNB!"""
     return "Hello HBNB!"
+
 
 if __name__ == "__main__":
 
