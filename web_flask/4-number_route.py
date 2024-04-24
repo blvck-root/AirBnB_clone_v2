bdc6fa000077b51e98c0758924ebc11a29895bed
sanieeme <shoneesani@gmail.com>
2024-04-24 15:05:38 +0000

    update number route

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -39,7 +39,7 @@ def python(text="is cool"):
 @app.route('/number/<n>', strict_slashes=False)
 def number(n):
     """displays n"""
-    return "{}".format(n)
+    return "{} is a number".format(n)
 
 
 if __name__ == "__main__":
